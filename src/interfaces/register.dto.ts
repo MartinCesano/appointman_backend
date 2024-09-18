62db1b9133cc84dbb37c67eaf91154b866408a5f
MartinCesano <cesano305@gmail.com>
2024-09-18 16:32:39 -0300

    Sincronizacion de cambios

--- src/interfaces/register.dto.ts
+++ src/interfaces/register.dto.ts
@@ -1,6 +1,18 @@
+import { IsEmail, IsNotEmpty, MinLength, IsString } from 'class-validator';
+
 export class RegisterDTO {
+  @IsEmail({}, { message: 'Email is invalid' })
   email: string;
+
+  @IsNotEmpty({ message: 'Password is required' })
+  @MinLength(8, { message: 'Password must be at least 6 characters long' })
   password: string;
+
+  @IsNotEmpty({ message: 'First name is required' })
+  @IsString({ message: 'First name must be a string' })
   firstName: string;
+
+  @IsNotEmpty({ message: 'Last name is required' })
+  @IsString({ message: 'Last name must be a string' })
   lastName: string;
 }
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors()
+
+  // Habilitar CORS
+  app.enableCors();
+
+  // Habilitar la validación global
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+
   await app.listen(3000);
 }
 bootstrap();
