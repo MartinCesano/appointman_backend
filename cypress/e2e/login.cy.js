174a8bad7f017db9196bf61f478534554eff709a
MartinCesano <cesano305@gmail.com>
2024-09-25 16:09:03 -0300

    Se modifico el archivo de test login

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,8 +1,8 @@
 
 describe('Register and Login flow', () => {
-  const baseUrl = 'http://localhost:3001'; // Cambia por la URL donde corra tu backend
-   
-  it.skip('Registro', () => {
+  const baseUrl = 'http://localhost:3000'; // Cambia por la URL donde corra tu backend
+
+  it('Registro', () => {
     cy.request('POST', `${baseUrl}/users/register`, {
       email: 'testing@gmail.com',
       password: 'password123',
@@ -13,7 +13,30 @@ describe('Register and Login flow', () => {
     });
   });
 
-  it.skip('Login', () => {
+  //register con datos en formato incorrecto
+  it('Registro incorrecto', () => {
+    cy.request({
+      method: 'POST',
+      url: `${baseUrl}/users/register`,
+      body: {
+        email: 'dosarrobas@@gmail.com',
+        password: 'corta',
+        firstName: '',
+        lastName: ''
+      },
+      failOnStatusCode: false // Evita que Cypress falle automáticamente en códigos de estado 4xx y 5xx
+    }).then((response) => {
+      expect(response.status).to.eq(400); // Verifica que el código de estado sea 400
+      expect(response.body).to.have.property('message').and.to.include.members([
+        'Email is invalid',
+        'Password must be at least 6 characters long',
+        'First name is required',
+        'Last name is required'
+      ]); // Verifica que el mensaje de error contenga los mensajes esperados
+    });
+  });
+
+  it('Login', () => {
     cy.request('POST', `${baseUrl}/users/login`, {
       email: 'testing@gmail.com',
       password: 'password123',
