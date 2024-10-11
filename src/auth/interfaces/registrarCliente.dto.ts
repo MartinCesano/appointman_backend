eb17c749c0237a2c26ef2d93e8bb5544ba63b98a
martingaidoo <martingaido00@gmail.com>
2024-10-11 16:16:13 -0300

    se crea el DTo de usuario

--- src/auth/interfaces/registrarCliente.dto.ts
+++ src/auth/interfaces/registrarCliente.dto.ts
@@ -1,23 +1,6 @@
 import { IsEmail, IsNotEmpty, MinLength, IsString, IsPhoneNumber } from 'class-validator';
+import {RegistrarUsuarioDTO} from "./registrarUsuario.dto";
 
-export class RegistrarClienteDTO {
-  @IsEmail({}, { message: 'Email is invalid' })
-  email: string;
+export class RegistrarClienteDTO extends RegistrarUsuarioDTO{
 
-  @IsNotEmpty({ message: 'Password is required' })
-  @MinLength(8, { message: 'Password must be at least 8 characters long' })
-  contrasena: string; 
-
-  @IsNotEmpty({ message: 'First name is required' })
-  @IsString({ message: 'First name must be a string' })
-  nombre: string; 
-
-  @IsNotEmpty({ message: 'Last name is required' })
-  @IsString({ message: 'Last name must be a string' })
-  apellido: string; 
-
-  @IsNotEmpty({ message: 'Phone number is required' })
-  telefono: string;
-
-  rol:string[];
 }
--- src/auth/interfaces/registrarEmpleado.dto.ts
+++ src/auth/interfaces/registrarEmpleado.dto.ts
@@ -1,23 +1,6 @@
 import { IsEmail, IsNotEmpty, MinLength, IsString, IsPhoneNumber } from 'class-validator';
+import {RegistrarUsuarioDTO} from "./registrarUsuario.dto";
 
-export class RegistrarEmpleadoDTO {
-  @IsEmail({}, { message: 'Email is invalid' })
-  email: string;
 
-  @IsNotEmpty({ message: 'Password is required' })
-  @MinLength(8, { message: 'Password must be at least 8 characters long' })
-  contrasena: string; 
-
-  @IsNotEmpty({ message: 'First name is required' })
-  @IsString({ message: 'First name must be a string' })
-  nombre: string; 
-
-  @IsNotEmpty({ message: 'Last name is required' })
-  @IsString({ message: 'Last name must be a string' })
-  apellido: string; 
-
-  @IsNotEmpty({ message: 'Phone number is required' })
-  telefono: string;
-
-  rol:string[];
+export class RegistrarEmpleadoDTO extends RegistrarUsuarioDTO{
 }
