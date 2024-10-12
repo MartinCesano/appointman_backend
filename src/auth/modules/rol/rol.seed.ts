e80590c1e3f526bd48054b7954904fbb10dce89f
martingaidoo <martingaido00@gmail.com>
2024-10-12 12:36:38 -0300

    se arregla el registro de los usuarios con su rol

--- src/auth/modules/rol/rol.seed.ts
+++ src/auth/modules/rol/rol.seed.ts
@@ -9,10 +9,10 @@ export class RolSeeder {
 
   async seedRoles() {
     const roles: DeepPartial<Rol>[] = [
-      { nombre: 'Cliente', description: 'Usuario de la pagina que solo reserva turnos' },
-      { nombre: 'Empleado', description: 'Usuario de la pagina que brinda servicios a una empresa y gestiona sus horarios' },
-      { nombre: 'Emprendedor', description: 'Usuario de la pagina propietario de una empresa' },
-      { nombre: 'Admin', description: 'Usuario de la pagina con permisos de administrador' },
+      { nombre: 'cliente', description: 'Usuario de la pagina que solo reserva turnos' },
+      { nombre: 'empleado', description: 'Usuario de la pagina que brinda servicios a una empresa y gestiona sus horarios' },
+      { nombre: 'emprendedor', description: 'Usuario de la pagina propietario de una empresa' },
+      { nombre: 'admin', description: 'Usuario de la pagina con permisos de administrador' },
 
     ];
 
--- src/auth/modules/usuario/usuario.service.ts
+++ src/auth/modules/usuario/usuario.service.ts
@@ -1,14 +1,14 @@
-import { HttpException, Injectable, UnauthorizedException, NotFoundException } from '@nestjs/common';
-import { LoginDTO } from '../../interfaces/login.dto';
-import { IUsuario } from '../../interfaces/usuario.interface';
-import { Usuario } from './usuario.entity';
-import { hashSync, compareSync } from 'bcrypt';
-import { JwtService } from '../jwt/jwt.service';
-import { DeepPartial, In, Repository, } from 'typeorm';
-import { PermisoService } from '../permiso/permiso.service';
-import { RolService } from '../rol/rol.service';
-import { RegistrarUsuarioDTO } from 'src/auth/interfaces/registrarUsuario.dto';
-import { RegistrarClienteDTO} from "../../interfaces/registrarCliente.dto";
+import {HttpException, Injectable, UnauthorizedException, NotFoundException} from '@nestjs/common';
+import {LoginDTO} from '../../interfaces/login.dto';
+import {IUsuario} from '../../interfaces/usuario.interface';
+import {Usuario} from './usuario.entity';
+import {hashSync, compareSync} from 'bcrypt';
+import {JwtService} from '../jwt/jwt.service';
+import {DeepPartial, In, Repository,} from 'typeorm';
+import {PermisoService} from '../permiso/permiso.service';
+import {RolService} from '../rol/rol.service';
+import {RegistrarUsuarioDTO} from 'src/auth/interfaces/registrarUsuario.dto';
+import {RegistrarClienteDTO} from "../../interfaces/registrarCliente.dto";
 import {ClienteService} from "../../../gestion-reserva-cliente/modules/cliente/cliente.service";
 import {Cliente} from "../../../gestion-reserva-cliente/modules/cliente/cliente.entity";
 import {ICliente} from "../../../gestion-reserva-cliente/interfaces/cliente.interface";
@@ -21,150 +21,148 @@ import {RegistrarEmpleadoDTO} from "../../interfaces/registrarEmpleado.dto";
 
 @Injectable()
 export class UsuarioService {
-  repository = Usuario
+    repository = Usuario
 
-  constructor(
-    private permissionsService: PermisoService,
-    private jwtService: JwtService,
-    private rolesService: RolService,
-  ) {}
-
-
-
-  async findUsers(): Promise<Usuario[]> {
-    try {
-      return await this.repository.find();
-    } catch (error) {
-      throw new HttpException('Find all users error', 500);
-    }
-  }
-
-  async updateUserById(id: number, user: DeepPartial<Usuario>): Promise<Usuario> {
-    const query = this.repository.createQueryBuilder('user')
-        .where('user.id = :id', { id });
-    const userActual = await query.getOne();
-
-    if (!userActual) {
-      throw new NotFoundException(`User with id ${id} not found`);
+    constructor(
+        private permissionsService: PermisoService,
+        private jwtService: JwtService,
+        private rolesService: RolService,
+    ) {
     }
 
-    this.repository.merge(userActual, user);
-    return await this.repository.save(userActual);
-  }
 
-  async deleteUserById(id: number): Promise<Usuario> {
-    const userRemove = await this.repository.findOneBy({ id });
-
-    if (!userRemove) {
-      throw new NotFoundException(`User with id ${id} not found`);
+    async findUsers(): Promise<Usuario[]> {
+        try {
+            return await this.repository.find();
+        } catch (error) {
+            throw new HttpException('Find all users error', 500);
+        }
     }
 
-    return await this.repository.remove(userRemove);
-  }
+    async updateUserById(id: number, user: DeepPartial<Usuario>): Promise<Usuario> {
+        const query = this.repository.createQueryBuilder('user')
+            .where('user.id = :id', {id});
+        const userActual = await query.getOne();
 
-  async refreshToken(refreshToken: string) {
-    return this.jwtService.refreshToken(refreshToken);
-  }
+        if (!userActual) {
+            throw new NotFoundException(`User with id ${id} not found`);
+        }
 
-  async canDo(usuario: IUsuario, nombrePermiso: string) {
-    let hasPermission = false;
-    if (usuario.roles.some(rol => rol.nombre === nombrePermiso)){
-      hasPermission = true;
-    }
-    if (!hasPermission) {
-      throw new HttpException('El usuario no tiene el Permiso', 401);
-    }
-    return hasPermission;
-  }
-
-  async registrar(dataUsuario: RegistrarUsuarioDTO) {
-    try {
-      const nuevoUsuario = new Usuario();
-        Object.assign(nuevoUsuario, dataUsuario);
-        nuevoUsuario.contrasena = hashSync(dataUsuario.contrasena, 10);
-        nuevoUsuario.roles = [];
-      return await this.repository.create(nuevoUsuario);
-    } catch (error) {
-      throw new HttpException('Error de creación', 500);
-    }
-  }
-
-
-  async login(body: LoginDTO) {
-    const user = await this.findByEmail(body.email);
-
-    if (!user) {
-      throw new UnauthorizedException('User not found');
+        this.repository.merge(userActual, user);
+        return await this.repository.save(userActual);
     }
 
-    const compareResult = compareSync(body.contrasena, user.contrasena);
+    async deleteUserById(id: number): Promise<Usuario> {
+        const userRemove = await this.repository.findOneBy({id});
 
-    if (!compareResult) {
-      throw new UnauthorizedException('Invalid password');
+        if (!userRemove) {
+            throw new NotFoundException(`User with id ${id} not found`);
+        }
+
+        return await this.repository.remove(userRemove);
     }
 
-    return {
-      accessToken: this.jwtService.generateToken({ email: user.email }),
-      refreshToken: this.jwtService.generateToken({ email: user.email }, 'refresh'),
-    };
-  }
-
-  async findByEmail(email: string): Promise<Usuario> {
-    return await this.repository.findOneBy({ email });
-  }
-
-  async assignPermissionToUser(idUsuario: number, body: { idPermiso: number }): Promise<Usuario> {
-    const user = await this.repository.findOne({
-      where: { id: idUsuario },
-      relations: ['permisos'],
-    });
-
-    if (!user) {
-      throw new NotFoundException(`User with ID ${idUsuario} not found`);
+    async refreshToken(refreshToken: string) {
+        return this.jwtService.refreshToken(refreshToken);
     }
 
-    const permission = await this.permissionsService.findPermissionById(body.idPermiso);
-
-    if (!permission) {
-      throw new NotFoundException(`Permission with ID ${body.idPermiso} not found`);
+    async canDo(usuario: IUsuario, nombrePermiso: string) {
+        let hasPermission = false;
+        if (usuario.roles.some(rol => rol.nombre === nombrePermiso)) {
+            hasPermission = true;
+        }
+        if (!hasPermission) {
+            throw new HttpException('El usuario no tiene el Permiso', 401);
+        }
+        return hasPermission;
     }
 
-    if (!user.permisos) {
-      user.permisos = [];
+    async registrar(dataUsuario: Usuario) {
+        try {
+            const nuevoUsuario = new Usuario();
+            Object.assign(nuevoUsuario, dataUsuario);
+            nuevoUsuario.contrasena = hashSync(dataUsuario.contrasena, 10);
+            return await this.repository.save(nuevoUsuario);
+        } catch (error) {
+            throw new HttpException('Error de creación', 500);
+        }
     }
 
-    user.permisos.push(permission);
-    await user.save();
 
-    return user;
-  }
+    async login(body: LoginDTO) {
+        const user = await this.findByEmail(body.email);
 
-  
+        if (!user) {
+            throw new UnauthorizedException('User not found');
+        }
 
-  async assignRoleToUser(userId: number, body: { roleId: number }): Promise<Usuario> {
-    const user = await this.repository.findOne({
-      where: { id: userId },
-      relations: ['roles'],
-    });
+        const compareResult = compareSync(body.contrasena, user.contrasena);
 
-    if (!user) {
-      throw new NotFoundException(`User with ID ${userId} not found`);
+        if (!compareResult) {
+            throw new UnauthorizedException('Invalid password');
+        }
+
+        return {
+            accessToken: this.jwtService.generateToken({email: user.email}),
+            refreshToken: this.jwtService.generateToken({email: user.email}, 'refresh'),
+        };
     }
 
-    const role = await this.rolesService.findRoleById(body.roleId);
-
-    if (!role) {
-      throw new NotFoundException(`Role with ID ${body.roleId} not found`);
+    async findByEmail(email: string): Promise<Usuario> {
+        return await this.repository.findOneBy({email});
     }
 
-    if (!user.roles) {
-      user.roles = [];
+    async assignPermissionToUser(idUsuario: number, body: { idPermiso: number }): Promise<Usuario> {
+        const user = await this.repository.findOne({
+            where: {id: idUsuario},
+            relations: ['permisos'],
+        });
+
+        if (!user) {
+            throw new NotFoundException(`User with ID ${idUsuario} not found`);
+        }
+
+        const permission = await this.permissionsService.findPermissionById(body.idPermiso);
+
+        if (!permission) {
+            throw new NotFoundException(`Permission with ID ${body.idPermiso} not found`);
+        }
+
+        if (!user.permisos) {
+            user.permisos = [];
+        }
+
+        user.permisos.push(permission);
+        await user.save();
+
+        return user;
     }
 
-    user.roles.push(role);
-    await user.save();
 
-    return user;
-  }
+    async assignRoleToUser(userId: number, body: { roleId: number }): Promise<Usuario> {
+        const user = await this.repository.findOne({
+            where: {id: userId},
+            relations: ['roles'],
+        });
+
+        if (!user) {
+            throw new NotFoundException(`User with ID ${userId} not found`);
+        }
+
+        const role = await this.rolesService.findRoleById(body.roleId);
+
+        if (!role) {
+            throw new NotFoundException(`Role with ID ${body.roleId} not found`);
+        }
+
+        if (!user.roles) {
+            user.roles = [];
+        }
+
+        user.roles.push(role);
+        await user.save();
+
+        return user;
+    }
 
 }
